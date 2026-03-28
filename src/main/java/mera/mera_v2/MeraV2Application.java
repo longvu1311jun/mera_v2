85caa791761475ab4120ee996ec7574c9ecad3c4
Jun <icloudcualong@gmail.com>
2026-03-28 11:02:11 +0700

    khởi tạo project

---
+++ src/main/java/mera/mera_v2/MeraV2Application.java
@@ -0,0 +1,13 @@
+package mera.mera_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeraV2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MeraV2Application.class, args);
+  }
+
+}
---
+++ src/test/java/mera/mera_v2/MeraV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package mera.mera_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeraV2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
